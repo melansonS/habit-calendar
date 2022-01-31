cb473579549f4e2634ccc87b944f62939f282a33
Sam Melanson <samuelmelanson@live.com>
2022-01-31 15:03:29 -0500

    first MUI styled-component typescript set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import ControlledSlyder from '@mui/material/Slider';
+import { Typography } from '@mui/material';
+import styled from 'styled-components';
+
+// Using styed components' styled function in conjuction with their transient props
+// which aren't pased down through to DOM elements ie; $customColor
+
+type CustomSliderProps = {
+  $customColor?: string,
+  ree?: number,
+}
+
+type MyOwnDivProps = {
+  textValue: string
+}
+
+const CustomSlider = styled(ControlledSlyder)({
+  background: 'red',
+});
+
+const CustomSliderTwo = styled(ControlledSlyder)((props : CustomSliderProps) => ({
+  color: props.$customColor,
+  width: '50%',
+  paddingLeft: props.ree || 0,
+}));
+
+function MyOwnDiv({ textValue } : MyOwnDivProps) {
+  return (<div>{textValue}</div>);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h3" gutterBottom>MUI Typescript Playground</Typography>
+        <ControlledSlyder defaultValue={23} />
+        <CustomSlider defaultValue={50} />
+        <CustomSliderTwo defaultValue={25} $customColor="violet" />
       </header>
+      <MyOwnDiv textValue="cool and interactive testing" />
     </div>
   );
 }
