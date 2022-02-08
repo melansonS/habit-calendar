af223b41e35b11ca4c199751bdd551130b79ab71
Sam Melanson <samuelmelanson@live.com>
2022-02-08 15:37:30 -0500

    higlight checkedDays and today :heavy_check_mark:

--- src/components/calendar/cells.tsx
+++ src/components/calendar/cells.tsx
@@ -6,12 +6,21 @@ import {
 } from 'date-fns';
 import {
   styled, Box, Typography,
+  useTheme as useMUITheme,
 } from '@mui/material';
 
 interface ICellsProps {
   currentMonth: Date
   today: Date
   isDarkMode: boolean
+}
+
+interface ICellProps {
+  isToday?: boolean
+  isChecked?: boolean
+  isDarkMode: boolean
+  primary: string
+  secondary: string
 }
 
 const CellsContainer = styled(Box)`
@@ -23,21 +32,24 @@ const Row = styled(Box)`
 const Cell = styled(Box)`
   height: 5rem;
   position: relative;
-  
+  background-color: ${({ isChecked, primary }:ICellProps) => (isChecked ? primary : 'inherit')};
+  border: ${({ isToday, secondary } :ICellProps) => (isToday ? `2px solid ${secondary}` : 'none')};
   :hover {
-    background-color: ${({ isDarkMode }: {isDarkMode: boolean}) => (isDarkMode ? '#333' : '#eee')};
+    background-color: ${({ isDarkMode }:ICellProps) => (isDarkMode ? '#333' : '#eee')};
   }
 `;
 
 export default function Cells({ currentMonth, today, isDarkMode }: ICellsProps) {
+  const { palette: { primary, secondary } } = useMUITheme();
   const monthStart = startOfMonth(currentMonth);
   const monthEnd = endOfMonth(monthStart);
   const startDate = startOfWeek(monthStart);
   const endDate = endOfWeek(monthEnd);
 
+  const checkedDays = ['1643518800000', '1643518800000', '1643778000000', '1643864400000', today.getTime().toString()];
+
   const dateFormat = 'd';
   const rows = [];
-
   let days = [];
   let day = startDate;
   let formattedDate = '';
@@ -47,13 +59,16 @@ export default function Cells({ currentMonth, today, isDarkMode }: ICellsProps) 
       formattedDate = format(day, dateFormat);
       days.push(
         <Cell
+          primary={primary.main}
+          secondary={secondary.main}
+          isChecked={checkedDays.includes(day.getTime().toString())}
           isDarkMode={isDarkMode}
+          isToday={today.getTime() === day.getTime()}
           key={`${day.getDate()}-key?`}
         >
           <Typography style={{ position: 'absolute', top: '0.75rem', right: '0.75rem' }}>
             {formattedDate}
           </Typography>
-          {today.getTime() === day.getTime() && <span>today??</span>}
         </Cell>,
       );
       day = addDays(day, 1);
